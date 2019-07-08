757ccfdb0b334609dcd47e5d881eac205efb400e
potenstop <40971383+potenstop@users.noreply.github.com>
2019-07-08 20:41:25 +0800

    Update ILogger.ts

--- src/log/ILogger.ts
+++ src/log/ILogger.ts
@@ -12,31 +12,31 @@ export interface ILogger {
     getName(): string;
     isTraceEnabled(): boolean;
     trace(msg: string): void;
-    trace(msg: string, args: any): void;
-    trace(msg: string, ...args: any[]): void;
     trace(msg: string, e: Error): void;
+    trace(msg: string, ...args: Object[]): void;
+    trace(msg: string, e: Error, ...args: Object[]): void;
 
     isDebugEnabled(): boolean;
     debug(msg: string): void;
-    debug(msg: string, args: any): void;
-    debug(msg: string, ...args: any[]): void;
     debug(msg: string, e: Error): void;
+    debug(msg: string, ...args: Object[]): void;
+    debug(msg: string, e: Error, ...args: Object[]): void;
 
     isInfoEnabled(): boolean;
     info(msg: string): void;
-    info(msg: string, args: any): void;
-    info(msg: string, ...args: any[]): void;
     info(msg: string, e: Error): void;
+    info(msg: string, ...args: Object[]): void;
+    info(msg: string, e: Error, ...args: Object[]): void;
 
     isWarnEnabled(): boolean;
     warn(msg: string): void;
-    warn(msg: string, args: any): void;
-    warn(msg: string, ...args: any[]): void;
     warn(msg: string, e: Error): void;
+    warn(msg: string, ...args: Object[]): void;
+    warn(msg: string, e: Error, ...args: Object[]): void;
 
     isErrorEnabled(): boolean;
     error(msg: string): void;
-    error(msg: string, args: any): void;
-    error(msg: string, ...args: any[]): void;
     error(msg: string, e: Error): void;
+    error(msg: string, ...args: Object[]): void;
+    error(msg: string, e: Error, ...args: Object[]): void;
 }
